21609153989245a335a9b4a3884d0d86b9466a6f
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-18 04:02:25 +1200

    learning python

---
+++ printing_printing_printing.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #new line character
+another_months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("Here are the months: ", another_months)
+
+print("""
+      There is something going on here.
+      With the three double-quotes.
+      We'll be able to type as much as we like.
+      """) #multiple lines
