7ce994c68a5ef685348651d132e9bf401e077d7d
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-19 16:09:19 +1200

    learning python

--- a.py
+++
@@ -1,4 +0,0 @@
-def func():
-    return "praise God"
-
-print(func())
--- function_can_return.py
+++ function_can_return.py
@@ -22,3 +22,40 @@ weight = multiply(90, 2)
 iq = divide(100, 2)
 
 print("Age: %d, Height: %d. Weight: %d, IQ: %d" % (age, height, weight,iq))
+
+def break_words(stuff):
+    """This function will break up words""" #documentation comment
+    words = stuff.split(" ")
+    return words
+
+def sort_words(words):
+    """This function will sort words"""
+    return sorted(words)
+
+def print_first_word(words):
+    """Print first word after popping it off"""
+    word = words.pop(0)
+    return word
+
+def print_last_word(words):
+    """Print first word after popping it off"""
+    word = words.pop(-1)
+    return word
+
+def sort_sentence(sentence):
+    """Takes in full sentence and return the sorted words"""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of a sentence"""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+    
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then print the first and last one"""
+    words = sort_sentence(words)
+    print_first_word(words)
+    print_last_word(words)
+    
