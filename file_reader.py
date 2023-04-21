1ef66c7356042c5712afc3dd0e4201b6c7af3d70
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 01:20:35 +1200

    learning python

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,10 @@
 with open("exp2.txt") as file_object:
     content = file_object.read()
-print(content)
+print(content.rstrip()) 
+
+print("-" * 10)
+filename = "exp2.txt"
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
