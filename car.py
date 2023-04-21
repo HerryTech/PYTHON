bb0f4e45dee4c8396dd68c29bf062a084ca2a02f
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 00:38:09 +1200

    learning python

--- car.py
+++ car.py
@@ -26,3 +26,29 @@ class Car:
         
     def fill_gas_tank(self):
         print(f"You can fill the gas for {self.make}")
+        
+class Battery:
+    def __init__(self, battery_size = 75):
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kwh battery")
+        
+    def get_range(self):
+        if self.battery_size <= 75:
+            range = 260
+            print(f"This car can go about {range} miles on a full charge.")
+        else:
+            range = 315
+            print(f"This car can go about {range} miles on a full charge.")
+            
+    
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+        
+        
+    def fill_gas_tank(self):
+        print("This car doesn't need a gas tank")
+    
--- my_car.py
+++ my_car.py
@@ -1,30 +1,5 @@
-from car import Car
+from car import *
 
-class Battery:
-    def __init__(self, battery_size = 75):
-        self.battery_size = battery_size
-        
-    def describe_battery(self):
-        print(f"This car has a {self.battery_size}-kwh battery")
-        
-    def get_range(self):
-        if self.battery_size <= 75:
-            range = 260
-            print(f"This car can go about {range} miles on a full charge.")
-        else:
-            range = 315
-            print(f"This car can go about {range} miles on a full charge.")
-            
-    
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery = Battery()
-        
-        
-    def fill_gas_tank(self):
-        print("This car doesn't need a gas tank")
-    
 my_car = Car("audi", "a4", 2019)
 print(my_car.descriptive_name())
 my_car.odometer_reading = 23_500 #modifying attribute directly
