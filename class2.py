b815280971f9c6a8cdcc4d56a73bf29f1486b7d4
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-21 23:08:14 +1200

    learning python

--- class2.py
+++ class2.py
@@ -92,11 +92,41 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 12
         
     def descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name
     
+    def read_odometer(self):
+        print(f"{self.make} car has {self.odometer_reading} miles on it")
+        
+    def update_odometer(self, mileage):
+        #self.odometer_reading = mileage
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+            
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+    
 my_car = Car("audi", "a4", 2019)
 print(my_car.descriptive_name())
+my_car.odometer_reading = 23 #modifying attribute directly
+my_car.read_odometer()
+
+my_car.update_odometer(10) #modifying attribute through a method
+my_car.read_odometer()
+
+print("-" * 10)
+
+my_used_car = Car("Subaru", "outback", 2015)
+print(my_used_car.descriptive_name())
+my_used_car.update_odometer(30)
+my_used_car.read_odometer()
+
+print("-" * 10)
+my_used_car.increment_odometer(50)
+my_used_car.read_odometer()
      
