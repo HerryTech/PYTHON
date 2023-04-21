6f3e88239d53897a97b711c32eecf01e0fd2cf18
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 00:32:18 +1200

    learning python

--- class.py
+++ class.py
@@ -151,34 +151,7 @@ user1.increment_login_attempt()
 print("-" * 10)
 user2.describe_user()
 
-class Car:
-    """An attempt to model a car"""
-    
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 12
-        
-    def descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name
-    
-    def read_odometer(self):
-        print(f"{self.make} car has {self.odometer_reading} miles on it")
-        
-    def update_odometer(self, mileage):
-        #self.odometer_reading = mileage
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-            
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
-        
-    def fill_gas_tank(self):
-        print(f"You can fill the gas for {self.make}")
+
         
 class Battery:
     def __init__(self, battery_size = 75):
