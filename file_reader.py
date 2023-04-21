93ea1ee273cf9d8c896e98745cdad4b29ccdf121
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 01:10:55 +1200

    learning python

---
+++ file_reader.py
@@ -0,0 +1,3 @@
+with open("exp2.txt") as file_object:
+    content = file_object.read()
+print(content)
--- reading_files.py
+++ reading_files.py
@@ -20,3 +20,4 @@ txt_again = open(file_again)
 
 print(txt_again.read())
 txt_again.close()
+
