c72163b2bfe3afe20e2cfe137530872d9be2f3f7
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 10:32:35 +1200

    learning python

--- file_reader.py
+++ file_reader.py
@@ -1,6 +1,26 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print("Here is your file %r" % filename)
+print(txt.read())
+txt.close()
+
+print("Type the filename again")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+txt_again.close()
+
+print("-" * 10 )
+
 with open("exp2.txt") as file_object:
     content = file_object.read()
-print(content.rstrip()) 
+print(content.strip()) 
 
 print("-" * 10)
 
@@ -8,7 +28,7 @@ filename = "exp2.txt"
 
 with open(filename) as file_object:
     for line in file_object:
-        print(line.rstrip())
+        print(line.strip())
         
 print("-" * 10)
 
@@ -18,5 +38,35 @@ with open(filename) as file_object:
 
 file = ''  
 for line in lines:
-    file += line.rstrip()
-    print(len(file))
+    file += line.strip()
+    print(file)
+    
+    
+print(f"{file[20:]}") 
+
+
+print(file)
+print(f"{file[:20]}") 
+print(len(file))
+
+print("-" * 10)
+
+filename = "pi.txt"
+pi_string = ""
+
+with open(filename) as pi:
+    lines = pi.readlines()
+
+for line in lines:
+    pi_string += line.strip()
+    
+    
+print(f"{pi_string[:50]}")
+
+birthday = input("Type in ypur birthday in mmddyy: ")
+
+if birthday in pi_string:
+    print("Your birthday appears in pi_string")
+else:
+    print("Your birthday does not appears in pi_string")
+
