f4906f8318edcea8c6cbf45de63dc477d4ffa6ce
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-22 12:27:04 +1200

    learning python

--- Exceptions/try_except.py
+++ Exceptions/try_except.py
@@ -1,2 +1,15 @@
-try:
-    print (5 / 0)
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("First number: ")
+    if first_number == "q":
+        break
+    second_number = input("Second number: ")
+    if second_number == "q":
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+        print(answer)
+    except ZeroDivisionError:
+        print("You can't divide by 0!. Try again or quit")
