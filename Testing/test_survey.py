ed88ff682fe3051ae103006e64019223c52a941b
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-23 14:42:55 +1200

    learning python

--- Testing/test_survey.py
+++ Testing/test_survey.py
@@ -4,23 +4,22 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Test for the class AnonymousSurvey"""
     
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods"""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+    
     def test_store_single_respones(self):
         """Test that a single response is stored properly."""
-        question = "What language did you first learn to speak?" 
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_responses("English")
-        self.assertIn("English", my_survey.responses)
+        self.my_survey.store_responses(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
         
     def test_store_more_respones(self):
         """Test that a all responses are stored properly."""
-        question = "What language did you first learn to speak?" 
-        my_survey = AnonymousSurvey(question)
-        responses = ["English", "Spanish", "French"]
-        for response in responses:
-            my_survey.store_responses(response)
-        
-        for response in responses:  
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_responses(response)
+            self.assertIn(response, self.my_survey.responses)
         
 if __name__ == "__main__":
     unittest.main()
