fb8d4722a95688c16b08ce96063cb7e9a27c03eb
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-23 13:42:36 +1200

    learning python

--- Testing/survey.py
+++ Testing/survey.py
@@ -8,3 +8,14 @@ class AnonymousSurvey:
         
     def show_question(self):
         """Show survey question"""
+        print(self.question)
+        
+    def store_responses(self, new_response):
+        """Store response to survey"""
+        self. responses.append(new_response)
+        
+    def show_result(self):
+        """Show all responses"""
+        print("Survey result:")
+        for response in self.responses:
+            print(response)
