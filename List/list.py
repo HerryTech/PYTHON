c818563d681dc35624a985945c736904c5f40916
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-23 15:18:44 +1200

    learning python

--- List/list.py
+++ List/list.py
@@ -1,3 +1,30 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1])
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
+
+
+print("-" * 10)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('honda')
+print(motorcycles)
+
+print("-" * 10)
+
+motorcycles = [] 
+motorcycles.append('honda') 
+motorcycles.append('yamaha') 
+motorcycles.append('suzuki') 
+print(motorcycles)
+
+motorcycles.insert(1, 'ducati')
+print(motorcycles)
