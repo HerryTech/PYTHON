329874773414e2455099bb68b9270ad061168815
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-24 17:12:18 +1200

    learning python

---
+++ File/dir.py
@@ -0,0 +1,15 @@
+import os
+
+"""Get name of currednt working directory"""
+print(os.getcwd())
+""""List content of directory"""
+print(os.listdir())
+"""Create a dir"""
+#os.mkdir("dirTest") 
+"""Remove directory"""
+#os.rmdir("dirTest") 
+"""Change into 'file' directory"""
+os.chdir("dirTest")
+
+
+
