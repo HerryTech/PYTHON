8ce372c668284bbafa51a2303b4225b2fce1f6a5
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-24 17:54:38 +1200

    learning python

--- File/path.py
+++ File/path.py
@@ -1,8 +1,24 @@
 from pathlib import Path
 
 """"Create a path"""
+p = Path("\\")
+print(p)
+print(f"\nDoes {p} exists: {p.exists()}")
+print(f"Is {p} a directory: {p.is_dir()}")
+print(f"Is {p} a file: {p.is_file()}")
+print(f"Is {p} absolute: {p.is_absolute()}")
+
+p = Path(".")
+print(p)
+print(f"\nDoes {p} exists: {p.exists()}")
+print(f"Is {p} a directory: {p.is_dir()}")
+print(f"Is {p} a file: {p.is_file()}")
+print(f"Is {p} absolute: {p.is_absolute()}")
+
 p = Path("newPath")
 print(p)
-print(f"Does {p} exists: {p.exists()}")
+print(f"\nDoes {p} exists: {p.exists()}")
 print(f"Is {p} a directory: {p.is_dir()}")
-print(f"Does {p} exists: {p.exists}")
+print(f"Is {p} a file: {p.is_file()}")
+print(f"Is {p} absolute: {p.is_absolute()}")
+
